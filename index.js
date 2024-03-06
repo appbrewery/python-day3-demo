afabb5842d7e33d0fc652a2e6791b025f970fa05
Angela Yu <8798027+angelabauer@users.noreply.github.com>
2024-03-06 10:13:29 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,10 @@ var CurrentId = undefined;
 
 var inputValues = [];
 const inputPrompts = [
-  `
-  You\'ve come to a lake. There is an island in the middle of the lake.
-  Type "wait" to wait for a boat. Type "swim" to swim across.
-  `,
-  `
-  You arrive at the island unharmed. There is a house with 3 doors.
-  One red, one yellow and one blue. Which colour do you choose?
-  `,
+  `You\'ve come to a lake. There is an island in the middle of the lake.
+  Type "wait" to wait for a boat. Type "swim" to swim across.`,
+  `You arrive at the island unharmed. There is a house with 3 doors.
+  One red, one yellow and one blue. Which colour do you choose?`,
 ];
 let isFirstClick = true;
 
@@ -47,10 +43,8 @@ Your mission is to find the treasure.
       false
     );
     NewLine(
-      `
-      You\'re at a cross road. Where do you want to go?
-      Type "left" or "right"
-      `,
+      `You\'re at a cross road. Where do you want to go?
+      Type "left" or "right"`,
       true
     );
   });
